a1a5711235aa81919a1d62908bcd29de8b036b78
Hyndhavi <Hyndhavi.Nagireddy@gmail.com>
2016-02-29 16:51:45 +0530

    Created the Main and Addition java files

---
+++ src/main/java/com/calc/Main.java
@@ -0,0 +1,22 @@
+package com.calc;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	
+		int a, b;
+		
+		Scanner scn = new Scanner(System.in);	
+		Addition AddObj=new Addition();
+		Substraction SubObj=new Substraction();
+			
+		System.out.println("\nEnter the values for a and b:-");
+		
+		a=scn.nextInt();
+		b=scn.nextInt();
+		
+		AddObj.add(a,b);
+		SubObj.sub(a,b);
+		
+		System.out.println("\nExecution Completed!");
+	}
+}
