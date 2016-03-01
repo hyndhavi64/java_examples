06291ea7c1324164e0a3e9fd1da1433f7fc2392e
Hyndhavi <Hyndhavi.Nagireddy@gmail.com>
2016-03-01 20:38:19 +0530

    deleted divisionand multi files

--- src/main/java/com/calc/Division.java
+++
@@ -1,8 +0,0 @@
-package com.calc;
-public class Division {
-	public void div(int a,int b) {
-		float c;
-		c=a / b;
-		System.out.println("Division of "+ a +" and "+ b +" is "+c);
-		}
-}
--- src/main/java/com/calc/Multiplication.java
+++
@@ -1,8 +0,0 @@
-package com.calc;
-public class Multiplication {
-	public void mul(int a,int b) {
-		int c;
-		c=a * b;
-		System.out.println("Multiplication of "+ a +" and "+ b +" is "+c);
-		}
-}
