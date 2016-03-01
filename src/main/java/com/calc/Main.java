3515c0cdd09a9f1b8f0367ecdaa117e97b031f67
Hyndhavi <Hyndhavi.Nagireddy@gmail.com>
2016-03-01 20:06:55 +0530

    uodated indexes

---
+++ src/main/java/com/calc/Main.java
@@ -0,0 +1,24 @@
+package com.calc;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	
+		int a, b;
+		
+		Scanner scn = new Scanner(System.in);	
+		Addition AddObj=new Addition();
+		Substraction SubObj=new Substraction();
+		Multiplication Mulobj=new Multiplication();
+			
+		System.out.println("\nEnter the values for a and b");
+		
+		a=scn.nextInt();
+		b=scn.nextInt();
+		
+		AddObj.add(a,b);
+		SubObj.sub(a,b);
+		Mulobj.mul(a,b);
+
+		System.out.println("\nExecution Completed!");
+	}
+}
