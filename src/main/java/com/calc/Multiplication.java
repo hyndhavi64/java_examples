45f5379e17f7fdd5a3727813969804a9f0142d9e
Hyndhavi <Hyndhavi.Nagireddy@gmail.com>
2016-03-01 20:12:13 +0530

    updated again

--- src/main/java/com/calc/Multiplication.java
+++ src/main/java/com/calc/Multiplication.java
@@ -1,5 +1,4 @@
 package com.calc;
-<<<<<<< HEAD
 public class Multiplication {
 	public void mul(int a,int b) {
 		int c;
@@ -7,12 +6,3 @@ public class Multiplication {
 		System.out.println("Multiplication of "+ a +" and "+ b +" is "+c);
 		}
 }
-=======
-public class Multiplicatio {
-	public void mul(int a,int b) {
-			int c;
-					c=a * b;
-							System.out.println("Multiplication of "+ a +" and "+ b +" is "+c);
-									}
-									}
->>>>>>> 518a2464e17e85a52b075416973dc7a56854c31a
